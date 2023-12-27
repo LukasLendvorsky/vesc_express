2ee80e0d2e8694ca8033806fed5aa3e657370978
Lukas Lendvorsky <lukas@lendvorsky.cz>
2023-12-27 01:14:31 +0100

    RGB led control, single strip

---
+++ main/onellight/led_rgb.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdint.h>
+#include <stdlib.h>
+
+void led_rgb_init();
+
+
+void led_strip_transmit(uint8_t *led_strip_pixels, size_t bytes);
+
+/**
+ * @brief Simple helper function, converting HSV color space to RGB color space
+ *
+ * Wiki: https://en.wikipedia.org/wiki/HSL_and_HSV
+ *
+ */
+void led_strip_hsv2rgb(uint32_t h, uint32_t s, uint32_t v, uint32_t *r, uint32_t *g, uint32_t *b);
