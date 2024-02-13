f53647a71940e9931e9b4fedcfe82392e0791274
Lukas Lendvorsky <lukas@lendvorsky.cz>
2024-02-14 00:02:10 +0100

    Switch between one SPI to refresh multiple strips

--- main/onellight/led_rgb.cpp
+++ main/onellight/led_rgb.cpp
@@ -1,6 +1,8 @@
 
 #include "led_rgb.hpp"
 #include "animations.hpp"
+#include "driver/gpio.h"
+#include "hal/gpio_types.h"
 extern "C"{
     #include "esp_log.h"
     #include "freertos/FreeRTOS.h"
@@ -8,6 +10,9 @@ extern "C"{
     #include "led_strip.h"
     #include "led_strip_spi.h"
     #include "led_strip_types.h"
+    #include "soc/spi_periph.h"
+    #include "esp_rom_gpio.h"
+    #include "soc/io_mux_reg.h"
 }
 
 
@@ -17,58 +22,58 @@ led_strip_spi_config_t spi_config = {
     .flags = { .with_dma = true}, // Using DMA can improve performance and help drive more LEDs
 };
 
-led_strip_config_t front_strip_cfg = {
+led_strip_config_t strip_cfg = {
     .strip_gpio_num = 7, // The GPIO that connected to the LED strip's data line
-    .max_leds = HEADLIGHT_NUM_LEDS, // The number of LEDs in the strip,
+    .max_leds = HEADLIGHT_NUM_LEDS > HID_NUM_LEDS ? HEADLIGHT_NUM_LEDS : HID_NUM_LEDS, // The number of LEDs in the strip,
     .led_pixel_format = LED_PIXEL_FORMAT_GRB, // Pixel format of your LED strip
     .led_model = LED_MODEL_WS2812, // LED strip model
     .flags = {.invert_out = false}, // whether to invert the output signal (useful when your hardware has a level inverter)
 };
 
+static constexpr auto front_led_strip_gpio = 7;
+static constexpr auto rear_led_strip_gpio = 8;
+static constexpr auto hid_led_strip_gpio = 6;
 
-
-led_strip_config_t rear_strip_cfg = {
-    .strip_gpio_num = 8, // The GPIO that connected to the LED strip's data line
-    .max_leds = HEADLIGHT_NUM_LEDS, // The number of LEDs in the strip,
-    .led_pixel_format = LED_PIXEL_FORMAT_GRB, // Pixel format of your LED strip
-    .led_model = LED_MODEL_WS2812, // LED strip model
-    .flags = {.invert_out = false}, // whether to invert the output signal (useful when your hardware has a level inverter)
-};
 
 Animation * front_animation;
 Animation * rear_animation;
 Animation * hid_animation;
 
-led_strip_config_t hid_strip_cfg = {
-    .strip_gpio_num = 6, // The GPIO that connected to the LED strip's data line
-    .max_leds = HID_NUM_LEDS, // The number of LEDs in the strip,
-    .led_pixel_format = LED_PIXEL_FORMAT_GRB, // Pixel format of your LED strip
-    .led_model = LED_MODEL_WS2812, // LED strip model
-    .flags = {.invert_out = false}, // whether to invert the output signal (useful when your hardware has a level inverter)
-};
-
 NullAnimation null_animation_front_rear = NullAnimation(HEADLIGHT_NUM_LEDS);
 NullAnimation null_animation_hid = NullAnimation(HID_NUM_LEDS);
 BlinkAnimation blink_animation = BlinkAnimation(10, 0, 100, 100);
 
-void update_strip(led_strip_config_t & strip_cfg, Animation & animation){
-    led_strip_handle_t handle;
-    ESP_ERROR_CHECK(led_strip_new_spi_device(&strip_cfg, &spi_config, &handle));
+static led_strip_handle_t handle;
+
+void update_strip(uint8_t strip_gpio, Animation & animation){
+    // connect GPIO of current led strip to 
+    gpio_iomux_out(strip_gpio, SPI2_FUNC_NUM, false);
     animation.update(handle);
-    ESP_ERROR_CHECK(led_strip_del(handle));
+    gpio_iomux_out(strip_gpio, PIN_FUNC_GPIO, false);
 }
 
 void led_rgb_init(){
     front_animation = &null_animation_front_rear;
     rear_animation = &null_animation_front_rear;
     hid_animation = &null_animation_hid;
+
+
+    ESP_ERROR_CHECK(led_strip_new_spi_device(&strip_cfg, &spi_config, &handle));
+
+    gpio_set_direction((gpio_num_t)front_led_strip_gpio, gpio_mode_t::GPIO_MODE_OUTPUT);
+    gpio_set_level((gpio_num_t)front_led_strip_gpio, 0);
+    gpio_set_direction((gpio_num_t)rear_led_strip_gpio, gpio_mode_t::GPIO_MODE_OUTPUT);
+    gpio_set_level((gpio_num_t)rear_led_strip_gpio, 0);
+    gpio_set_direction((gpio_num_t)hid_led_strip_gpio, gpio_mode_t::GPIO_MODE_OUTPUT);
+    gpio_set_level((gpio_num_t)hid_led_strip_gpio, 0);
+    
 }
 
 
 void led_rgb_update_animations(){
-    update_strip(front_strip_cfg,front_animation ? *front_animation : null_animation_front_rear);
-    update_strip(rear_strip_cfg, rear_animation ? *rear_animation : null_animation_front_rear);
-    update_strip(hid_strip_cfg, hid_animation ? *hid_animation : null_animation_hid);
+    update_strip(front_led_strip_gpio,front_animation ? *front_animation : null_animation_front_rear);
+    update_strip(rear_led_strip_gpio, rear_animation ? *rear_animation : null_animation_front_rear);
+    update_strip(hid_led_strip_gpio, hid_animation ? *hid_animation : null_animation_hid);
 }
 
 void led_rgb_set_animation(LedRgbType type, Animation *anim)
